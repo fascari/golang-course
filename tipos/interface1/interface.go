fcdb8ff85c571e23c97737889d993030bc983b47
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-12-24 10:47:46 -0300

    interfaces (testes)

--- tipos/interface1/interface.go
+++ tipos/interface1/interface.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type imprimivel interface {
 	toString() string
+	toStringTeste() string
 }
 
 type pessoa struct {
@@ -16,9 +20,17 @@ type produto struct {
 	preco float64
 }
 
+func (p produto) toStringTeste() string {
+	return p.toString()
+}
+
 // interfaces são implementadas implicitamente
 func (p pessoa) toString() string {
 	return p.nome + " " + p.sobrenome
+}
+
+func (p pessoa) toStringTeste() string {
+	return p.toString()
 }
 
 func (p produto) toString() string {
@@ -26,16 +38,15 @@ func (p produto) toString() string {
 }
 
 func imprimir(x imprimivel) {
-	fmt.Println(x.toString())
+	fmt.Println(reflect.TypeOf(x), "- toString() -", x.toString())
+	fmt.Println(reflect.TypeOf(x), "- toStringTeste() -", x.toStringTeste())
 }
 
 func main() {
 	var coisa imprimivel = pessoa{"Roberto", "Silva"}
-	fmt.Println(coisa.toString())
 	imprimir(coisa)
 
 	coisa = produto{"Calça Jeans", 79.90}
-	fmt.Println(coisa.toString())
 	imprimir(coisa)
 
 	p2 := produto{"Calça Jeans", 179.90}
